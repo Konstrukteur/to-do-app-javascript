6a55c299411209ba66cef20ee01615cc31fea692
KONSTRUKTEUR <raumerkunder@gmail.com>
2023-02-14 17:30:14 +0100

    Initial setup of edit and delete buttons

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
-const addButton = document.querySelector('#add-task-button')
-const clearButton = document.querySelector('#clear-button')
+const addButton = document.querySelector('#add-task-button');
+const editButton = document.querySelector('#edit-task-button');
+const deleteButton = document.querySelector('#delete-task-button');
+const clearButton = document.querySelector('#clear-button');
 
 class Item {
     constructor(taskID, taskText) {
@@ -41,6 +43,44 @@ class Item {
         document.getElementById(this.taskID).appendChild(deleteTaskButton);
         // update counter
         document.getElementById('pending-tasks').innerText = Item.countTasks();
+    }
+
+    // add delete todo task
+    deleteItem() {
+        localStorage.setItem(this.taskID, this.taskText);
+        // // create initial div tag
+        // let taskItem = document.createElement('div');
+        // taskItem.setAttribute('class', 'list-input-box-task');
+        // taskItem.setAttribute('id', this.taskID);
+        // document.getElementById('todo-list').appendChild(taskItem);
+        // // create input box
+        // let taskBox = document.createElement('input')
+        // taskBox.setAttribute('type', 'text');
+        // taskBox.setAttribute('class', 'input-filed');
+        // taskBox.setAttribute('id', this.taskID + '-text');
+        // taskBox.setAttribute('value', this.taskText);
+        // document.getElementById(this.taskID).appendChild(taskBox);
+        // // create edit button
+        // let editTaskButton = document.createElement('input');
+        // editTaskButton.setAttribute('type', 'button');
+        // editTaskButton.setAttribute('class', 'btn-sub');
+        // editTaskButton.setAttribute('id', this.taskID + '-edit-button')
+        // editTaskButton.setAttribute('value', 'Edit');
+        // document.getElementById(this.taskID).appendChild(editTaskButton);
+        // // create delete button
+        // let deleteTaskButton = document.createElement('input');
+        // deleteTaskButton.setAttribute('type', 'button');
+        // deleteTaskButton.setAttribute('class', 'btn-sub');
+        // deleteTaskButton.setAttribute('id', this.taskID + '-edit-button')
+        // deleteTaskButton.setAttribute('value', 'Delete');
+        // document.getElementById(this.taskID).appendChild(deleteTaskButton);
+        // // update counter
+        // document.getElementById('pending-tasks').innerText = Item.countTasks();
+    }
+
+    // add edit todo task
+    editItem() {
+
     }
 
     static destroy() {
